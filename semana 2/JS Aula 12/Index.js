9be8437f6c1d1cb75f655dab29a854ad02d7c6d8
Leonardo Boldrin <69554628+Lboldrin95@users.noreply.github.com>
2021-10-26 22:20:14 -0300

    1.0

--- semana 2/JS Aula 12/Index.js
+++ semana 2/JS Aula 12/Index.js
@@ -1,7 +1,9 @@
-let olderPersonName = prompt ("Qual é o seu nome da pessoa mais velha?")
+/*let olderPersonName = prompt ("Qual é o seu nome da pessoa mais velha?")
 let olderPersonAge = prompt ("Qual é a sua idade da pessoa mais velha?")
 
 let youngerPersonName = prompt ("Qual é o nome da pessoa mais nova?")
 let youngerPersonAge = prompt ("Qual é a idade da pessoa mais nova?")
 
-let ageDifference = olderPersonAge - youngerPersonAge
+let ageDifference = olderPersonAge - youngerPersonAge*/
+
+console.log("Olá Leonardo")
