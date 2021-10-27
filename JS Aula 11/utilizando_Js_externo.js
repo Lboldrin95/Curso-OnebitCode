e8558304e8827124f8d16632b157eeed48e31b72
Leonardo Boldrin <69554628+Lboldrin95@users.noreply.github.com>
2021-10-26 22:10:32 -0300

    1.0
    integração

---
+++ JS Aula 11/utilizando_Js_externo.js
@@ -0,0 +1,5 @@
+ alert("Bem vindo! A seguir pediremos algumas informações.")    
+ 
+ let name = prompt ("Qual o seu nome?")
+
+ 
---
+++ JS Aula 12/Index.js
@@ -0,0 +1,7 @@
+let olderPersonName = prompt ("Qual é o seu nome da pessoa mais velha?")
+let olderPersonAge = prompt ("Qual é a sua idade da pessoa mais velha?")
+
+let youngerPersonName = prompt ("Qual é o nome da pessoa mais nova?")
+let youngerPersonAge = prompt ("Qual é a idade da pessoa mais nova?")
+
+let ageDifference = olderPersonAge - youngerPersonAge
