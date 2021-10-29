be48becd215da1038aa263186cc778317c4feebe
Leonardo Boldrin <69554628+Lboldrin95@users.noreply.github.com>
2021-10-28 23:41:52 -0300

    1.0

--- semana 2/JS Aula 10/Hello_world.js
+++ semana 2/JS Aula 10/Hello_world.js
@@ -1,1 +1,9 @@
-alert("Meu código JS externo ")
+alert("Bem-vindo! A seguir pediremos que informe alguns dados... ")
+let Name = prompt ("Qual é o seu nome? ")
+let Age = prompt ("Qual é a sua idade? ")
+
+let  ageConfirmation = confirm("sua idade é " + Age + " anos" )
+
+alert(
+    "Nome do usuário: " + Name + "\n\nIdade: " + Age +"\n\nIdade Confirmada: " + ageConfirmation
+    )
