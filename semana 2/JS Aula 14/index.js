91853be7ce8cff1d5db0a7f2d26d116db7d85967
Leonardo Boldrin <69554628+Lboldrin95@users.noreply.github.com>
2021-10-29 07:48:44 -0300

    1.0

---
+++ semana 2/JS Aula 14/index.js
@@ -0,0 +1,8 @@
+let pessoaMaisVelhaNome = prompt("Qual é o nome da pessoa mais velha?")
+let pessoaMaisVelhaIdade = prompt("Qual é a idade da pessoa mais velha?")
+let pessoaMaisNovaNome = prompt("Qual é o nome da pessoa mais nova?")
+let pessoaMaisNovaIdade = prompt("Qual é a idade da pessoa mais nova?")
+let idadeDiferença = pessoaMaisVelhaIdade - pessoaMaisNovaIdade
+alert(
+    "O nome da pessoa mais velha é: " + pessoaMaisVelhaNome + "\nA idade da pessoa mais velha é: " + pessoaMaisVelhaIdade + "\nO nome da pessoa mais nova é: " + pessoaMaisNovaNome + "\nA idade da pessoa mais nova é: " + pessoaMaisNovaIdade + "\nA diferença entre idades é de: " + idadeDiferença
+    )
---
+++ semana 2/JS Aula 15/index.js
@@ -0,0 +1,10 @@
+let spaceship = "Golias"
+let velocity = 80 
+console.log(velocity == 90)
+console.log(spaceship == "Golias")
+console.log(velocity == "80")
+console.log(velocity != 80)
+console.log(velocity != 90)
+console.log(velocity !== 80)
+console.log(velocity > 70)
+console.log(velocity > 80)
