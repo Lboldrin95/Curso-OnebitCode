2f1f4f608744d605eafacb0675dee4b7568a95b9
Leonardo Boldrin <69554628+Lboldrin95@users.noreply.github.com>
2021-10-28 22:45:57 -0300

    1.0

---
+++ semana 2/JS Aula 09/index.js
@@ -0,0 +1,13 @@
+let spaceship = "FE Supernova"
+console.log(spaceship)
+spaceship = "FE Helmet"
+console.log(spaceship)
+
+const squad = "Estelar"
+console.log(squad)
+/*a variavel const é uma variavel única, que não recebe outro valor dps de ser formada*/ 
+
+var velocity = 80
+console.log(velocity)
+velocity = 20
+console.log(velocity)
