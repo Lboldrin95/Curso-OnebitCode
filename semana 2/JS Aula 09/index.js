e1d5657865d04914a7f7b91dd9d2bc7e5891547e
Leonardo Boldrin <69554628+Lboldrin95@users.noreply.github.com>
2021-10-28 23:08:28 -0300

    1.0

--- semana 2/JS Aula 09/index.js
+++ semana 2/JS Aula 09/index.js
@@ -11,3 +11,12 @@ var velocity = 80
 console.log(velocity)
 velocity = 20
 console.log(velocity)
+
+let noValue = "Eu tenho valor"
+console.log(noValue)
+
+let departureConfirmation = Confirm("Nave pronta para decolar. Aguardando Confirmação!")
+console.log(departureConfirmation)
+
+let pilot = prompt("Qual o seu nome, piloto?")
+console.log(pilot)
