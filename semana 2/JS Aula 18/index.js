959b8e58d326338330c8c03adaba167b1fe3e4d5
Leonardo Boldrin <69554628+Lboldrin95@users.noreply.github.com>
2021-11-02 10:40:42 -0300

    1.0

---
+++ semana 2/JS Aula 18/index.js
@@ -0,0 +1,5 @@
+let velocity = prompt(Qual a velocidade desejada?) 
+
+if(velocity < 100) {
+    console.log("Estamos numa velocidade aceitável")
+    } else { console.log("Entrando em velocidade risco")}
